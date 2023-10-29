c94146ace70497493ea9cf320151f01cc3e2c070
Maxxafari <max.berkenstam@gmail.com>
2023-10-29 10:01:56 +0100

    filter gamepad zero point

--- src/lib/components/control/ControlWithStore.svelte
+++ src/lib/components/control/ControlWithStore.svelte
@@ -4,15 +4,36 @@
 	import GamePad from './GamePad.svelte';
 	let ls = 0;
 	let rs = 0;
+	let lsFilter = 0;
+	let rsFilter = 0;
+	function filterMinimumInput(lsRaw: number, rsRaw: number) {
+		if (lsRaw < 11 && lsRaw > -11) lsFilter = 0;
+		else lsFilter = lsRaw;
+		if (rsRaw < 11 && rsRaw > -11) rsFilter = 0;
+		else rsFilter = rsRaw;
 
-	$: updSteer({ lm: ls, rm: rs });
+		updSteer({ lm: lsFilter, rm: rsFilter });
+	}
+	$: filterMinimumInput(ls, rs);
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<div>
-	<!-- <div>{ls} || {rs}</div> -->
+<div class="wrap">
+	<div class="filtered"><span>{lsFilter}</span><span>{rsFilter}</span></div>
 	<GamePad bind:ls bind:rs />
 </div>
 
 <style>
+	.wrap {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	.filtered {
+		display: flex;
+		width: 100%;
+		height: 30px;
+		justify-content: space-around;
+	}
 </style>
